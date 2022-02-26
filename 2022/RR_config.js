7c511d89b5723f1c6aa2153a6788fd9c1c7b683e
bmurphy5924 <38738484+bmurphy5924@users.noreply.github.com>
2022-02-26 15:32:43 -0800

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        //"defaultValue":"2022 SFR",//
+        "defaultValue":"2022 SFR",
         "required":"true",
         //"disabled":"false"
       },
